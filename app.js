b6b2906fae8faaba32a7657fe94dccc661f1baaf
Sourav Gope <137763197+souravgope@users.noreply.github.com>
2023-11-24 08:54:49 +0530

    Added a html file

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+   $('.slider').slick({
+         arrows:false,
+         dots:true,
+         appendDots: '.slider-dot',
+         dotsClass: 'dot',
+   });
+
+
+   let hamberger = document.querySelector('.hamberger');
+
+   let times = document.querySelector('.times');
+
+   let mobileNav = document.querySelector('.mobile-nav');
+
+
+   hamberger.addEventListener('click',function(){
+      mobileNav.classList.add('open');
+   });
+
+   times.addEventListener('click',function(){
+      mobileNav.classList.remove('open');
+   });
+
+
+});
+
+
